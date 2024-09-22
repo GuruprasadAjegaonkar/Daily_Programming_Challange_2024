1cae20c94d904ccf3fd75576adf64d0607000eed
Guruprasad Ajegaonkar <68501124+GuruprasadAjegaonkar@users.noreply.github.com>
2024-09-22 22:58:34 +0530

    Add files via upload

---
+++ 11.Permutation_string.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    void solve(vector<int> nums, vector<vector<int>>& ans, int index) {
+        if (index >= nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        for (int j = index; j < nums.size(); j++) {
+            swap(nums[index], nums[j]);
+            solve(nums, ans, index + 1);
+            swap(nums[index], nums[j]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(nums, ans, index);
+        return ans;
+    }
+};
+int main() {
+    Solution obj;
+    vector<int> nums = {1, 2, 3};
+    vector<vector<int>> result = obj.permute(nums);
+    for (const auto& perm : result) {
+        for (int num : perm) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
